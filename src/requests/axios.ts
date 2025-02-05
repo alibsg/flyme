671a04a44f1cc9d644fdf3ba621709324040c1b7
alibsg <30772100+alibsg@users.noreply.github.com>
2025-02-05 17:32:11 +0300

    fixing base url change issue

--- src/requests/axios.ts
+++ src/requests/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_RAPID_API_BASE_URL;
-const API_HEADERS: {[key: string]: string} = {
+const API_HEADERS: { [key: string]: string } = {
   'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
   'x-rapidapi-host': import.meta.env.VITE_RAPID_API_HOST,
 };
@@ -10,13 +10,8 @@ export const axiosInstance = axios.create({ baseURL: API_BASE_URL });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    for(const key of Object.keys(API_HEADERS)) {
+    for (const key of Object.keys(API_HEADERS)) {
       config.headers.set(key, API_HEADERS[key]);
-
-    }
-    
-    if (config.url?.startsWith('/api')) {
-      config.baseURL = '';
     }
     return config;
   },
